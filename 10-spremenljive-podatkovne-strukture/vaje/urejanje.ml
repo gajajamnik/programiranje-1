3a7071882dab08755991e5da6b4c24dfa8ab2d25
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-12-14 22:49:31 +0100

    danasnje vaje

--- 10-spremenljive-podatkovne-strukture/vaje/urejanje.ml
+++ 10-spremenljive-podatkovne-strukture/vaje/urejanje.ml
@@ -9,6 +9,12 @@
  val l : int list = [0; 1; 0; 4; 0; 9; 1; 2; 5; 4]
 [*----------------------------------------------------------------------------*)
 
+let rec randlist len max =
+    if len <= 0 then
+        []
+    else 
+        let x = Random.int max in  
+        x :: (randlist (len - 1) max)
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Sedaj lahko s pomočjo [randlist] primerjamo našo urejevalno funkcijo (imenovana
@@ -18,7 +24,9 @@
  let test = (randlist 100 100) in (our_sort test = List.sort compare test);;
 [*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
 
-
+let default_test our_sort =
+    let test = randlist 100 100 in 
+    our_sort test = List.sort compare test
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
  Urejanje z Vstavljanjem
 [*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
@@ -35,13 +43,22 @@
  - : int list = [7]
 [*----------------------------------------------------------------------------*)
 
+let rec insert y list = match list with
+    | [] -> y :: []
+    | x :: xs -> 
+        if y <= x then y :: x :: xs
+        else x :: (insert y xs)
 
 (*----------------------------------------------------------------------------*]
  Prazen seznam je že urejen. Funkcija [insert_sort] uredi seznam tako da
  zaporedoma vstavlja vse elemente seznama v prazen seznam.
 [*----------------------------------------------------------------------------*)
 
+let rec insert_sort list = match list with
+    | [] -> []
+    | x :: xs -> insert x (insert_sort xs)
 
+let insert_sort_pro = List.fold_left (fun list x -> insert x list) []
 
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
  Urejanje z Izbiranjem
@@ -53,6 +70,37 @@
  pojavitvijo elementa [z]. V primeru praznega seznama vrne [None]. 
 [*----------------------------------------------------------------------------*)
 
+let min_and_rest list =
+    let rec najdi_min trenutni_min list = match list with
+        | [] -> trenutni_min
+        | x :: xs ->
+            if x < trenutni_min then najdi_min x xs (* x postane nov minimum *)
+            else najdi_min trenutni_min xs
+    in
+    let rec odstrani y list = match list with
+        | [] -> []
+        | x :: xs ->
+            if x = y then xs
+            else x :: (odstrani y xs)
+    in
+    match list with 
+    | [] -> None
+    | x :: xs -> 
+        let m = najdi_min x xs in 
+        Some (m, odstrani m (x::xs))
+
+(*IDEJA: sprehodis se po seznamu tako da na vsakem koraku glavo (x) postavis za trenutni minimum in primerjas z vrednostmi v preostanku *)
+
+let min_and_rest_pro list = match list with
+    | [] -> None 
+    | x :: xs -> 
+        let min_x = List.fold_left min x xs in
+        let rec remove_min list = match list with
+            | [] -> []  
+            | y :: ys when y = min_x -> ys
+            | y :: ys -> y :: (remove_min ys)
+        in 
+        Some (min_x, remove_min list)
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Pri urejanju z izbiranjem na vsakem koraku ločimo dva podseznama, kjer je prvi
@@ -65,6 +113,9 @@
  saj vedno prenesemo najmanjšega. Tako vemo, da moramo naslednji najmanjši člen
  dodati na konec urejenega podseznama.
  (Hitreje je obrniti vrstni red seznama kot na vsakem koraku uporabiti [@].)
+
+
+ GLEJ ANIMACIJO: https://visualgo.net/bn/sorting
 [*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
 
 (*----------------------------------------------------------------------------*]
@@ -72,7 +123,13 @@
  Namig: Uporabi [min_and_rest] iz prejšnje naloge.
 [*----------------------------------------------------------------------------*)
 
-
+let rec selection_sort list =
+    let rec selection_loop sorted_list list =  (*sorted list tu nastopa kot neke vrste akumulator *)
+        match min_and_rest list with 
+        | None -> List.rev sorted_list         (* ko pride do konca vrnes obrnjen 'akumulator' *)
+        | Some (min, rest) -> selection_loop (min :: sorted_list) rest
+    in                                          (* ^^^  vrednosti nalagas v akumulator *)
+    selection_loop [] list
 
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
  Urejanje z Izbiranjem na Tabelah
@@ -101,14 +158,24 @@
  - : int array = [|0; 4; 2; 3; 1|]
 [*----------------------------------------------------------------------------*)
 
+let swap tab i j =
+    let prvi = tab.(i) in
+    tab.(i) <- tab.(j);   (* <-- ce hoces zaporedno izvest ukaze ki vracajo unit moras dat podpicje *)
+    tab.(j) <- prvi
 
 (*----------------------------------------------------------------------------*]
  Funkcija [index_min a lower upper] poišče indeks najmanjšega elementa tabele
  [a] med indeksoma [lower] and [upper] (oba indeksa sta vključena).
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- index_min [|0; 2; 9; 3; 6|] 2 4 = 4
+ index_min [|0; 2; 9; 3; 6|] 2 4 = 3
 [*----------------------------------------------------------------------------*)
 
+let rec index_min a lower upper =
+    if upper - lower < 0 then failwith "napacna indeksa"
+    else if lower = upper then lower 
+    else 
+        if a.(lower) < a.(index_min a (lower + 1) upper) then lower 
+        else index_min a (lower + 1) upper
 
 (*----------------------------------------------------------------------------*]
  Funkcija [selection_sort_array] implementira urejanje z izbiranjem na mestu. 
@@ -116,4 +183,15 @@
  Namig: Za testiranje uporabi funkciji [Array.of_list] in [Array.to_list]
  skupaj z [randlist].
 [*----------------------------------------------------------------------------*)
+
+let selection_sort_array a =
+    let len = Array.length a in
+    let rec sort_loop (boundary : int) =
+        if boundary >= len then ()    (*vrnes unit () *)
+        else 
+            let i_min = index_min a boundary (len - 1) in  
+            swap a boundary i_min;  (* premaknes kazalec *)
+            sort_loop (boundary + 1) 
+        in 
+        sort_loop 0
 

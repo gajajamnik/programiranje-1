722333cef2688c301693300813535f292baf6e75
Gaja Jamnik <gaja.jamnik@gmail.com>
2021-01-19 21:20:08 +0100

    kolokvij

--- izpiti/2017-12-20/kolokvij-2017-12-20.ml
+++ izpiti/2017-12-20/kolokvij-2017-12-20.ml
@@ -4,28 +4,36 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame par in zamenja komponenti para.
    Primer: /obrni (2, 4) = (4, 2)/ *)
- let obrni = failwith "dopolni me"
+ let obrni (x, y) = (y, x)
 
 (* 1.2) Definirajte funkcijo, ki vzame par p in vrednost x in zamenja drugo
    komponento para p z x.
    Primer: /zamenjaj_drugo (2, 4) 7 = (2, 7)/ *)
- let zamenjaj_drugo = failwith "dopolni me"
+ let zamenjaj_drugo p x =
+   let p1, p2 = p in
+   p1, x
 
 (* 1.3) Definirajte funkcijo, ki vzame seznam parov in izračuna nov seznam parov,
    ki imajo drugo komponento zamenjano z 42.
    Primer: /vsem_zamenjaj_drugo_z_42 [(12, 1); (2, 4)] = [(12, 42); (2, 42)]/ *)
- let vsem_zamenjaj_drugo_z_42 = failwith "dopolni me"
+ let rec vsem_zamenjaj_drugo_z_42 list = match list with 
+   | [] -> []
+   | (x,y)::xs -> (x,42)::(vsem_zamenjaj_drugo_z_42 xs)
 
 (* 1.4) Definirajte funkcijo, ki varno vrne glavo seznama v primeru, ko seznam ni prazen.
    Uporabite tip option.
    Primer: /glava [1; 2; 3] = Some 1/ *)
- let glava = failwith "dopolni me"
+ let glava list = match list with
+   | [] -> None
+   | x::xs -> Some x
 
 (* 1.5) Definirajte funkcijo, vzame funkcijo (f: 'a -> 'b), neko vrednost (x : 'a) in
    celo število n. Funkcija naj vrne vrednost, ki jo dobimo če f n-krat uporabimo na x,
    torej f (f ... (f x)...).
    Primer: /uporabi_veckrat succ 0 420 = 420/ *)
- let uporabi_veckrat = failwith "dopolni me"
+ let rec uporabi_veckrat f x n = match n with
+   | 0 -> x 
+   | n -> uporabi_veckrat f (f x) (n - 1)
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
@@ -36,7 +44,7 @@
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
+type 'a drevo = Rose of 'a * 'a drevo list
 
 (* 2.2) Definirajte naslednja rožna drevesa:
 
@@ -47,28 +55,40 @@ type 'a drevo = DopolniMe
 
  *)
 
-let t = failwith "dopolni me"
-let t' = failwith "dopolni me"
-let t'' = failwith "dopolni me"
+let t = Rose (1, [])
+let t' = Rose (2, [Rose (1, []); Rose (1, [])])
+let t'' = Rose (3, [Rose(-1, []); Rose (2, [Rose (1, []); Rose (1, [])]); Rose (0, [])])
 
 (* 2.3) Definirajte funkcijo, ki vrne gozd rožnega drevesa. *)
-let vrni_gozd = failwith "dopolni me"
+let vrni_gozd rozno_drevo =
+   let Rose (_, list) = rozno_drevo in
+   list
 
 (* 2.4) Definirajte funkcijo, ki izpiše vse vrednosti v rožnem drevesu celih števil.
    Števila naj bodo v ločenih vrsticah. Uporabite (print_int : int -> unit) in
    (print_newline : unit -> unit). *)
-let izpisi_vrednosti = failwith "dopolni me"
+
 
 (* 2.5) Definirajte funkcijo, ki izračuna globino rožnega drevesa, t.j. dolžino
    najdaljše poti od korena do lista. *)
-let globina = failwith "dopolni me"
+let rec globina (Rose(_, l)) =
+      let seznam_globin gozd = List.map globina l in
+      let najglobji sez = List.fold_left max 0 sez in
+      1 + (najglobji (seznam_globin l)) 
+      
 
 (* 2.6) Definirajte funkcijo, ki sestavi (poljubno) rožno drevo globine n.
    Vrednosti v korenih so poljubne. *)
-let globoko_drevo = failwith "dopolni me"
+let rec globoko_drevo n = match n with
+   | 0 -> failwith "ne obstaja"
+   | 1 -> Rose(1, [])
+   | n -> Rose(n, [globoko_drevo (n-1)])
 
 (* 2.7) Definirajte funkcijo, ki sprejme funkcijo (f : 'b -> 'a -> 'b) in začetno vrednost (acc : 'b)
    in funkcijo f zloži [fold] preko drevesa (t : 'a drevo). Vrstni red pri tem ni pomemben.
+
+   t' = Rose (2, [Rose (1, []); Rose (1, [])])
+   t'' = Rose (3, [Rose(-1, []); Rose (2, [Rose (1, []); Rose (1, [])]); Rose (0, [])])
 
    Za primer t' želimo vrednost f (f (f acc 1) 2) 2)  (lahko tudi f (f (f acc 2) 1) 2))
    Primer: /zlozi (fun acc x -> x + acc) 0 t'' = 6/
@@ -78,4 +98,11 @@ let globoko_drevo = failwith "dopolni me"
    Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
    na primer List.map, niso repno rekurzivne, zato se jim raje
    izognite. *)
-let zlozi = failwith "dopolni me"
+let rec zlozi f acc drevo = match drevo with
+   | Rose (x, []) -> f acc x
+   | Rose (x, list) -> 
+      let rec iz_seznama acc list = match list with
+         | [] -> x + acc
+         | y::ys ->  iz_seznama (zlozi f acc drevo) ys
+      in
+      iz_seznama acc list
